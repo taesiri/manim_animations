6a78b59260901de1afda30e65e7ce43a30f8bef7
Mohammad Reza Taesiri <mtaesiri@gmail.com>
2023-07-02 14:36:46 -0600

    Update simple-image-grid.py

--- ImageGrid/simple-image-grid.py
+++ ImageGrid/simple-image-grid.py
@@ -1,9 +1,27 @@
 from manim import *
 from PIL import Image
+import numpy as np
+from functools import partial
 import torchvision.transforms.functional as fv
 import torchvision.transforms as transforms
-from functools import partial
-import numpy as np
+
+
+def crop_at(size, slice_x, slice_y):
+    def slice_crop(image, size, slice_x, slice_y):
+        width, height = image.size
+        tile_size_x = width // 3
+        tile_size_y = height // 3
+        anchor_x = (slice_y * tile_size_x) + (tile_size_x // 2)
+        anchor_y = (slice_x * tile_size_y) + (tile_size_y // 2)
+        return fv.crop(
+            image,
+            anchor_y - (size // 2),
+            anchor_x - (size // 2),
+            size,
+            size,
+        )
+
+    return partial(slice_crop, size=size, slice_x=slice_x, slice_y=slice_y)
 
 
 class ImageToGrid(Scene):
@@ -20,28 +38,29 @@ class ImageToGrid(Scene):
         self.play(FadeIn(img_obj))
         self.wait()
 
-        # Slice the image into 9 patches using PIL
+        # Slice the image into 9 patches using torchvision.transforms and the crop_at function
         img = Image.open(img_path)
-        width, height = img.size
-        patch_width = width // 3
-        patch_height = height // 3
-        patches = []
+
+        zoom_scales = [256, 512, 768, 1024]
+
+        patch_mobs = []
         for i in range(3):
             for j in range(3):
-                left = j * patch_width
-                upper = i * patch_height
-                right = left + patch_width
-                lower = upper + patch_height
-                patch = img.crop((left, upper, right, lower))
-                patches.append(patch)
+                zoom_transform = transforms.Compose(
+                    [
+                        transforms.Resize(
+                            zoom_scales[0],
+                            interpolation=transforms.InterpolationMode.BICUBIC,
+                        ),
+                        crop_at(224, i, j),
+                    ]
+                )
+                patch = zoom_transform(img)
+                patch_np = np.array(patch)
+                patch_mob = ImageMobject(patch_np)
+                patch_mob.scale_to_fit_height(5 / 3)
+                patch_mobs.append(patch_mob)
 
-        # Replace the original one with the 9 patches
-        patch_mobs = []
-        for patch in patches:
-            patch_np = np.array(patch)
-            patch_mob = ImageMobject(patch_np)
-            patch_mob.scale_to_fit_height(5 / 3)
-            patch_mobs.append(patch_mob)
         self.remove(img_obj)
 
         # Initial positions of each patch
@@ -94,5 +113,31 @@ class ImageToGrid(Scene):
 
         # Play all animations
         self.play(*animations, run_time=2)
+
+        # Animate zoom scale transitions
+        for scale in zoom_scales[1:]:
+            new_patch_mobs = []
+            for i in range(3):
+                for j in range(3):
+                    zoom_transform = transforms.Compose(
+                        [
+                            transforms.Resize(
+                                scale,
+                                interpolation=transforms.InterpolationMode.BICUBIC,
+                            ),
+                            crop_at(224, i, j),
+                        ]
+                    )
+                    patch = zoom_transform(img)
+                    patch_np = np.array(patch)
+                    patch_mob = ImageMobject(patch_np)
+                    patch_mob.scale_to_fit_height(5 / 3)
+                    patch_mob.move_to(patch_mobs[i * 3 + j].get_center())
+                    new_patch_mobs.append(patch_mob)
+
+            # Animate transition to new patches
+            self.play(*[FadeOut(old) for old in patch_mobs], run_time=0.5)
+            self.play(*[FadeIn(new) for new in new_patch_mobs], run_time=0.5)
+            patch_mobs = new_patch_mobs
 
         self.wait()
